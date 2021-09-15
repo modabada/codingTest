65e9a05ea4170e8a3a97641fe2297aaf130baf56
modabada <miw1102@naver.com>
2021-09-15 12:03:33 +0900

    commit lotto algorithm

--- lotto/lotto.py
+++ lotto/lotto.py
@@ -1,11 +1,5 @@
 # 로또
-def highest(lottos, win):
-    for el in win:
-        if el not in lottos:
-            if 0 in lottos:
-                del lottos[lottos.index(0)]
-            else:
-                pass
-
-
-print("ㅎㅇ")
+def solution(lottos, win_nums):
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    intersection = len(set(lottos) & set(win_nums))
+    return rank[intersection + lottos.count(0)], rank[intersection]
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = [2, 3, 4]
+    print(tuple(a) & tuple(b))
