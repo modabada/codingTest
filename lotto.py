11626ce39fdd47bf58570a3413f5df80da3bf5b3
modabada <miw1102@naver.com>
2021-09-15 11:25:06 +0900

    Initial commit

---
+++ lotto.py
@@ -0,0 +1,11 @@
+# 로또
+def highest(lottos, win):
+    for el in win:
+        if el not in lottos:
+            if 0 in lottos:
+                del lottos[lottos.index(0)]
+            else:
+                pass
+
+
+print("ㅎㅇ")
