d6cffac404438afc6934fda66c9614082adb95d1
modabada <miw1102@naver.com>
2021-09-20 13:43:51 +0900

    commit number and str algorithm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import numAndStr.number as n
 
 if __name__ == "__main__":
-    en = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    # n = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     print(n.solution('nineeightsevensixfivefourthreetwoonezero'))
