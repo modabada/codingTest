af62c931a66404f69d83d84896220f3153418dfa
modabada <miw1102@naver.com>
2021-09-29 19:50:23 +0900

    commit function development algorithm

--- algorithm_lv2/functionDevelopment/main.py
+++ algorithm_lv2/functionDevelopment/main.py
@@ -1,3 +1,12 @@
 def solution(progresses, speeds):
     answer = []
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            cnt += 1
+            del progresses[0]
+        if cnt != 0:
+            answer.append(cnt)
     return answer
