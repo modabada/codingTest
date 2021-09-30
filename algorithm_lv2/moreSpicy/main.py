eafb12712dc3a4ee4415b3e5c695e61929f2ca7a
modabada <miw1102@naver.com>
2021-09-30 19:43:29 +0900

    commit make more spicy algorithm

--- algorithm_lv2/moreSpicy/main.py
+++ algorithm_lv2/moreSpicy/main.py
@@ -1,10 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42626
+import heapq
+
+
 def solution(scoville, K):
-    scoville.sort(reverse=True)
+    H = []
+    for e in scoville:
+        heapq.heappush(H, e)
     cnt = 0
-    while len(scoville) > 1:
+    while H[0] < K and len(H) > 1:
         cnt += 1
-        if scoville[-1] + scoville[-2] * 2 > K:
-            return cnt
-        scoville.pop()
-        scoville.pop()
-    return -1
+        mixed = heapq.heappop(H) + heapq.heappop(H) * 2
+        heapq.heappush(H, mixed)
+    return -1 if H[0] < K else cnt
--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from algorithm_lv2.moreSpicy import main as test
 
 if __name__ == "__main__":
     testSet = [
-        [1, 2, 3, 9, 10, 12]
+        [1, 2, 3, 9, 10, 12],
+        [1, 2, 3, 4, 5, 6],
+        [0, 0, 0, 0, 0]
     ]
     testSet2 = [
-        7
+        7,
+        1000,
+        1
     ]
     """
     answer
