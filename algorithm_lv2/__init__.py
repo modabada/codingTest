93bb7d3fe92f3817ecab678c206b0f91d7fc5ded
modabada <69136135+modabada@users.noreply.github.com>
2021-10-13 02:15:36 +0900

    commit printer algorithm

--- algorithm_lv2/__init__.py
+++ algorithm_lv2/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
     'camouflage', 'countOf124', 'fixBracket', 'functionDevelopment', 'moreSpicy', 'keepDistance',
     'lightCycle', 'maximumEXP', 'menuRenewal', 'newsClustering', 'normalRect',
-    'openChatting', 'phoneBook', 'removeThatPair', 'strCompression', 'targetNumber',
+    'openChatting', 'printer', 'phoneBook', 'removeThatPair', 'strCompression', 'targetNumber',
     'tuple'
 ]
