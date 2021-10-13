fa4e7c829a8bb458d3c8c3af58a8517094318e8c
modabada <69136135+modabada@users.noreply.github.com>
2021-10-14 03:47:00 +0900

    commit bestAlbum algorithm

---
+++ algorithm_lv3/bestAlbum/main.py
@@ -0,0 +1,5 @@
+from hash.bestAlbum import main as bestAlbum
+
+
+def solution(genres, plays):
+    return bestAlbum.solution(genres, plays)
--- hash/__init__.py
+++ hash/__init__.py
@@ -1,3 +1,3 @@
 __all__ = [
-    'camouflage', 'cannotComplete', 'phoneBook'
+    'bestAlbum', 'camouflage', 'cannotComplete', 'phoneBook'
 ]
