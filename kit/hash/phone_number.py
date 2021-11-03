ae2938769c275c753cb65b0e7ed6aef943108356
문인우 <miw1102@naver.com>
2021-11-03 18:44:15 +0900

    commit phone number algorithm

---
+++ kit/hash/phone_number.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42577
+
+
+def solution(phone_book):
+    phone_book.sort()
+    tmp = phone_book[0]
+    for i in range(1, len(phone_book)):
+        if phone_book[i][:len(tmp)] == tmp:
+            return False
+        tmp = phone_book[i]
+    return True 
