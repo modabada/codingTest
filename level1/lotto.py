ef5706ae1d6256fd434050e906e94a9798d53cac
문인우 <miw1102@naver.com>
2021-11-03 19:31:53 +0900

    commit lotto algorithm

---
+++ level1/lotto.py
@@ -0,0 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/77484
+def solution(lottos, win_nums):
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    intersection = len(set(lottos) & set(win_nums))
+    return rank[intersection + lottos.count(0)], rank[intersection]
