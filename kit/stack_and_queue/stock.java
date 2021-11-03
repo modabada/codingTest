5f6e489d595e376df70d30071c168d1a90fdff3a
문인우 <miw1102@naver.com>
2021-11-03 18:58:34 +0900

    commit stock algorithm

---
+++ kit/stack_and_queue/stock.java
@@ -0,0 +1,19 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42584?language=java#
+
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length; i++) {
+            int cnt = 0;
+            for(int j = i + 1; j < prices.length; j++) {
+                cnt++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+}
