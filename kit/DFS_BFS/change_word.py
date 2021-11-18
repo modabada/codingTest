6117e1f2533ea82adb1748ee106d95fe7af88f36
문인우 <miw1102@naver.com>
2021-11-19 03:34:11 +0900

    commit change word algorithm
    completed

--- kit/DFS_BFS/change_word.py
+++ kit/DFS_BFS/change_word.py
@@ -1,6 +1,31 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43163
+from collections import deque
 
 
 def solution(begin, target, words):
-    answer = 0
-    return answer
+    if target not in words:
+        return 0
+
+    visited = list()
+    queue = deque()
+    queue.append((begin, 0))
+
+    while queue:
+        element = queue.popleft()
+        visited.append(element[0])
+        for word in words:
+            cnt = 0
+            for x, y in zip (word, element[0]):
+                if x != y:
+                    cnt += 1
+            if cnt == 1:
+                if word == target:
+                    return element[1] + 1
+                elif word[0] not in visited:
+                    print(element[1])
+                    queue.append((word, element[1] + 1))
+    return 0
+
+
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]), 4)
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]), 0)
