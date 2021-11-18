9cf1480a761a9315476757b42a6f1d9a15723d62
문인우 <miw1102@naver.com>
2021-11-19 03:12:19 +0900

    commit target number algorithm
    completed

---
+++ kit/DFS_BFS/target_number.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43165
+
+
+def solution(numbers, target):
+    answer = 0
+    cases = [0]
+    for num in numbers:
+        ar = []
+        for case in cases:
+            ar.append(case + num)
+            ar.append(case - num)
+        cases = ar
+    for case in cases:
+        if case == target:
+            answer += 1
+    return answer
