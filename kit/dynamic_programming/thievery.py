09b2427e48fb6031cc075dadd16ea53cd051d953
문인우 <miw1102@naver.com>
2021-11-22 07:10:38 +0900

    edit thievery algorithm
    not completed

--- kit/dynamic_programming/thievery.py
+++ kit/dynamic_programming/thievery.py
@@ -1,22 +1,27 @@
 def solution(money):
     answer = 0
-    for first in [False, True]:
-        dp = [[0, 0] for i in range(len(money))]
+    for first in [True, False]:
+        # poket[n] = [n번째 집을 안턴 경우, 턴 경우]
+        poket = [[0, 0] for _ in range(len(money))]
         if first:
-            dp[0][1] = money[0]
+            poket[0][1] = money[0]
 
-        for i in range(1, len(money) - 1):
-            dp[i][0] = max(dp[i-1])
-            dp[i][1] = dp[i-1][0] + money[i]
+        for i in range(1, len(money)):
+            # 안털 경우 -> 이전 경우에서 큰 값을 찾으면 됨
+            poket[i][0] = max(poket[i - 1])
+            # 털 경우 -> 이전 경우에서 안턴 경우 + 현재 집을 턴 값
+            poket[i][1] = poket[i - 1][0] + money[i]
 
-        if first:  # 첫번째집 턴경우 마지막집은 못턴다.
-            dp[-1][0] = max(dp[-2][0], dp[-2][1])
-            dp[-1][1] = 0
-        else:
-            dp[-1][0] = 0
-            dp[-1][1] = dp[-2][0] + money[-1]
-        answer = max(answer, max(dp[-1]))
+        # 첫 집을 털면, 마지막 집을 못턺
+        if first:
+            poket[-1][1] = 0
+
+        answer = max(answer, max(poket[-1]))
     return answer
 
 
+# poket[n] = [[첫집O n집O, 첫집O n집X], [첫집X n집O, 첫집X n집X]]
+# 위 방식으로 배열을 크게잡고 반복횟수 줄이기 -> 실패,
+# 메모리공간때문에 느린것이라면 배열 사이즈를 작게, 반복횟수를 많이 해서 해결 가능할수도
 print(solution([0, 0, 0, 3, 0, 3 ,4]))
+print(solution([1, 2, 3, 1]))
