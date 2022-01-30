49444570ade28dbc071000873eeca850bd197ef5
문인우 <miw1102@naver.com>
2022-01-30 21:40:45 +0900

    commit stepping stone algorithm not complete

---
+++ kit/binary_search/step_stone.py
@@ -0,0 +1,10 @@
+def solution(distance, rocks, n):
+    rocks.sort()
+    min_dis = 1
+    max_dis = distance
+    mid = 0
+    while min_dis <= max_dis:
+        mid = (min_dis + max_dis) // 2
+
+    answer = 0
+    return answer
